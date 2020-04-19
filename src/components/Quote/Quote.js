d2f30e84ddf8e1f93791b6d3a23e10c88c63d926
EYE MOTION <eyemotiongamestudio@gmail.com>
2020-04-19 22:51:12 +0400

    update

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -5,18 +5,69 @@ import {
     FormattedNumber } from 'react-intl'
 import defaultMessage from './Messages'
 
+
 function Quote() {
     return (
         <div className="container">
-            <FormattedMessage
-                id="title"
-                defaultMessage={defaultMessage.quote.title}
-            />
-            <FormattedMessage
-                id="text"
-                defaultMessage=
-                {defaultMessage.quote.text}
-            />
+            <div className="flex-row">
+                <div className="flex-col" 
+                    style={{ padding: '0 20px 0 10px', flex: 1 }}>
+
+                </div>
+                <div className="flex-col">
+                    ...........
+                </div>
+            </div>    
+            <h1>  
+                <FormattedMessage
+                    id="title"
+                    defaultMessage={defaultMessage.quote.title}
+                />
+            </h1>
+            <p style={{ fontSize: 20}}>
+                <FormattedMessage
+                    id="text"
+                    defaultMessage=
+                    {defaultMessage.quote.text}
+                />
+            </p>
+            <p>
+                <FormattedMessage
+                    id="person"
+                    defaultMessage=
+                    {defaultMessage.quote.person}
+                    
+                />
+            </p>
+            <div className="flex-col"
+                style={{ 
+                    width: 300, 
+                    padding: 20,
+                    backgroundColor: '#eee'}}
+            >
+                <FormattedDate
+                    value={new Date()}
+                    year='numeric'
+                    month='long'
+                    day='numeric'
+                    weekday='long'
+                />
+                <br/>        
+                <FormattedNumber
+                    value={10000}
+                    style='unit'
+                    unit='kilobyte'
+                    unitDisplay='narrow'
+                />
+                <br/>
+                <FormattedNumber
+                    value={10000000}
+                    style='unit'
+                    unit='fahrenheit'
+                    unitDisplay='narrow'
+                />           
+
+            </div>
         </div>
     )
 }
